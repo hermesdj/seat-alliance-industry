c7d558dcf712ce776ab78d763549780bae863dc0
Enolyo <l.bryan@hotmail.fr>
2024-01-22 11:34:10 +0100

    traduction de ai-order
    manque la ligne 34 et 14 à traduire

--- src/resources/lang/fr/ai-orders.php
+++ src/resources/lang/fr/ai-orders.php
@@ -1,62 +1,62 @@
 <?php
 
 return [
-    'create_order_title' => 'Create Order',
+    'create_order_title' => 'Créer un ordre',
 
-    'order_title' => 'Order',
-    'orders_title' => 'Orders',
-    'your_orders_title' => 'Your Orders',
+    'order_title' => 'Ordre',
+    'orders_title' => 'Ordres',
+    'your_orders_title' => 'Vos ordres',
 
-    'repeating_order_title' => 'Repeating Order',
-    'repeating_order_desc' => 'This is a order repeating itself every :days days. The next repetition will be published on the :date.',
+    'repeating_order_title' => 'Ordre répété',
+    'repeating_order_desc' => 'Ceci est un ordre qui se republie lui-même chaque :days jours. La prochaine republication au lieu le :date.'
 
     'items' => 'Items',
     'items_placeholder' => "MULTIBUY:\nTristan 100\nOmen 100\nTritanium 30000\n\nFITTINGS:\n[Pacifier, 2022 Scanner]\n\nCo-Processor II\nCo-Processor II\n\nMultispectrum Shield Hardener II\nMultispectrum Shield Hardener II\n\nSmall Tractor Beam II\nSmall Tractor Beam II",
-    'split_items' => 'Split items into separate orders',
+    'split_items' => 'Séparer les items en différents ordres.',
 
-    'reward_label' => 'Reward %',
-    'reward_hint' => 'The minimal profit is :mpp',
-    'add_profit_to_manual_prices_label' => 'Add Reward to Manual Prices',
+    'reward_label' => 'Récompense en %',
+    'reward_hint' => 'Le profit minimum est de :mpp',
+    'add_profit_to_manual_prices_label' => 'Ajouter une récompense au prix manuel',
 
-    'days_to_complete_label' => 'Days to complete',
+    'days_to_complete_label' => 'Jour pour compléter',
 
     'location_label' => 'Location',
-    'priority_label' => 'Priority',
-    'priority_Very Low' => 'Very Low',
-    'priority_Low' => 'Low',
+    'priority_label' => 'Priorité',
+    'priority_Very Low' => 'Très basse',
+    'priority_Low' => 'basse',
     'priority_Normal' => 'Normal',
-    'priority_Preferred' => 'Preferred',
+    'priority_Preferred' => 'Priorité',
     'priority_Important' => 'Important',
-    'priority_Critical' => 'Critical',
+    'priority_Critical' => 'Critique',
 
     'seat_inventory_label' => 'Seat-Inventory',
-    'seat_inventory_hint' => 'Add as source to seat-inventory',
+    'seat_inventory_hint' => 'Ajouter une source à seat-inventory',
     'seat_inventory_desc' => 'As soon as a delivery for this order is created, a item source will be added to seat-inventory. Once the delivery is marked as completed, the source will be removed. The source will be added to the <u>first</u> workspace containing <code>add2allianceindustry</code> at any position in it\'s name. You can rename workspaces <a href=":route">here.</a>',
 
-    'repetition_label' => 'Repetition',
-    'repetition_never' => 'Never',
-    'repetition_weekly' => 'Weekly',
-    'repetition_every_two_weeks' => 'Every 2 weeks',
-    'repetition_monthly' => 'Monthly',
+    'repetition_label' => 'Répétition',
+    'repetition_never' => 'Jamais',
+    'repetition_weekly' => 'Hebdomadaire',
+    'repetition_every_two_weeks' => 'Bi-hebdomadaire',
+    'repetition_monthly' => 'Mensuel',
 
-    'add_order_btn' => 'Add Order',
+    'add_order_btn' => 'Ajouter un ordre',
 
-    'invalid_order_label' => 'invalid order',
+    'invalid_order_label' => 'Ordre invalide',
 
-    'close_order_btn' => 'Close this Order',
-    'update_price_btn' => 'Update Price',
-    'update_price_action' => 'update the price? Manual prices will be overwritten!',
-    'extend_time_btn' => 'Extend Time',
-    'extend_time_action' => ' want to expand the time to deliver by 1 week',
+    'close_order_btn' => 'Fermer cette ordre',
+    'update_price_btn' => 'Mettre à jour le prix',
+    'update_price_action' => 'Mettre à jour le prix ? Le prix manuel sera écrasé !',
+    'extend_time_btn' => 'Ajouter du temps',
+    'extend_time_action' => ' souhaite ajouter 1 semaine au temps de livraison',
 
     // Marketplace
-    'order_marketplace_title' => 'Order Marketplace',
-    'open_orders_title' => 'Open Orders',
+    'order_marketplace_title' => 'Ordre du marché',
+    'open_orders_title' => 'Ouvrir les ordres',
 
-    'close_all_completed_orders_btn' => 'Close all completed Orders',
+    'close_all_completed_orders_btn' => 'Fermer tout les ordres complétés',
 
-    'create_order_success' => 'Successfully added new order',
-    'update_time_success' => 'Extended the time!',
-    'update_price_success' => 'Updated the price!',
-    'close_order_success' => 'Successfully closed order!',
+    'create_order_success' => 'Ajout d\'un nouvel ordre réussi !',
+    'update_time_success' => 'Temps ajouté !',
+    'update_price_success' => 'Prix mis à jour !',
+    'close_order_success' => 'Fermeture de l\'ordre réussi !',
 ];
